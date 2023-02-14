d31043ff4457704582c57944970aee673830258e
raylu15742cs <raylucs15742@gmail.com>
2023-02-14 10:49:32 -0800

    update

--- lesson_05-frontend/src/features/users/UsersList.js
+++ lesson_05-frontend/src/features/users/UsersList.js
@@ -1,4 +1,15 @@
+import { useGetUsersQuery } from "./usersApiSlice"
+
 const UsersList = () => {
+
+    const {
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetUsersQuery()
+    
     return (
         <h1>UsersList</h1>
     )
