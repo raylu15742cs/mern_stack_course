7823a46bc727cd8f009410948d460ef15ab58531
raylu15742cs <raylucs15742@gmail.com>
2023-02-14 11:34:45 -0800

    update

--- lesson_05-frontend/src/features/users/UsersList.js
+++ lesson_05-frontend/src/features/users/UsersList.js
@@ -9,9 +9,38 @@ const UsersList = () => {
         isError,
         error
     } = useGetUsersQuery()
-    
-    return (
-        <h1>UsersList</h1>
-    )
+
+    let content 
+
+    if ( isLoading) content = <p>Loading...</p>
+
+    if ( isError ) {
+        content = <p className="errmsg">{error?.data?.message}</p>
+    }
+
+    if (isSuccess) {
+        const { ids } = users
+
+        const tableContent = ids?.length
+            ? ids.map(userId => <User key={userId} userId={userId} />) 
+            : null
+
+        content = (
+            <table className="table table--users">
+                <thead className="table__thead">
+                    <tr>
+                        <th scopre="col" className="table__th user__username">UserName</th>
+                        <th scopre="col" className="table__th user__roles">Roles</th>
+                        <th scopre="col" className="table__th user__edit">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
+        )
+    }
+
+    return content
 }
 export default UsersList
