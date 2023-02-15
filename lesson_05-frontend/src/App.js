afa0d45ab19d49d10f1f8736e49f2ac8bdd27590
raylu15742cs <raylucs15742@gmail.com>
2023-02-14 16:12:17 -0800

    udpate

--- lesson_05-frontend/src/App.js
+++ lesson_05-frontend/src/App.js
@@ -10,6 +10,7 @@ import EditUser from './features/users/EditUser';
 import NewUserForm from './features/users/NewUserForm';
 import EditNote from './features/notes/EditNote';
 import NewNote from './features/notes/NewNote';
+import Prefetch from './features/auth/Prefetch';
 
 function App() {
   return (
@@ -17,23 +18,24 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+        <Route element={<Prefetch />}>
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
 
-        <Route path="dash" element={<DashLayout />}>
-          <Route index element={<Welcome />} />
+            <Route path="users">
+              <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUser />} />
+              <Route path="new" element={<NewUserForm />} />
+            </Route>
 
-          <Route path="users">
-            <Route index element={<UsersList />} />
-            <Route path=":id" element={<EditUser />} />
-            <Route path="new" element={<NewUserForm />} />
+            <Route path="notes">
+              <Route index element={<NotesList />} />
+              <Route path=":id" element={<EditNote />} />
+              <Route path="new" element={<NewNote />} />
+            </Route>
           </Route>
-
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-            <Route path=":id" element={<EditNote />} />
-            <Route path="new" element={<NewNote />} />
-          </Route>
+          {/* End Dash */}
         </Route>
-        {/* End Dash */}
       </Route>
     </Routes>
   );
