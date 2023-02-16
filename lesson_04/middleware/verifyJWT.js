d4eee870db68595eabe3c61eb3e50964cbc2e0f8
raylu15742cs <raylucs15742@gmail.com>
2023-02-16 10:39:52 -0800

    update

--- lesson_04/middleware/verifyJWT.js
+++ lesson_04/middleware/verifyJWT.js
@@ -6,6 +6,19 @@ const verifyJWT = (req, res, next) => {
     if (!authHeader?.startsWith('Bearer')) {
         return res.status(401).json({ message: 'Unauthorized' })
     }
+
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if(err) return res.status(403).json({ message: 'Forbidden' })
+            req.user = decoded.UserInfo.username;
+            req.roles = decoded.UserInfo.roles;
+            next()
+        }
+    )
 }
 
 module.exports = verifyJWT
