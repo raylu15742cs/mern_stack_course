8a8b379e32ae82d32754b7812569df0c32d3f338
raylu15742cs <raylucs15742@gmail.com>
2023-02-17 12:56:43 -0800

    update note

--- lesson_05-frontend/src/features/notes/Note.js
+++ lesson_05-frontend/src/features/notes/Note.js
@@ -2,14 +2,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
-import { useSelector } from "react-redux";
-import { selectNoteById } from "./notesApiSlice";
-
-import React from 'react'
+import { useGetNotesQuery } from "./notesApiSlice";
 
 const Note = ({ noteId}) => {
 
-    const note = useSelector(state => selectNoteById(state, noteId))
+    const { note } = useGetNotesQuery("notesList", {
+      selectFromResult: ({ data }) => ({
+        note: data?.entities[noteId]
+      }),
+    })
 
     const navigate = useNavigate()
 
