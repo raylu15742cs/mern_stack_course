720c39149c0b8b430010e8149ab199272ea2968b
raylu15742cs <raylucs15742@gmail.com>
2023-02-17 12:53:33 -0800

    update

--- lesson_05-frontend/src/features/auth/Prefetch.js
+++ lesson_05-frontend/src/features/auth/Prefetch.js
@@ -5,10 +5,10 @@ import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Prefetch = () => {
+  
   useEffect(() => {
     store.dispatch(notesApiSlice.util.prefetch('getNotes', 'notesList', { force: true}))
     store.dispatch(usersApiSlice.util.prefetch('getUsers', 'usersList', { force: true}))
-    
   }, []);
 
   return <Outlet />;
